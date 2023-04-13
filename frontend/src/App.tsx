1fdd22e9daf2d219053fe95039fa0d10171b0476
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-04-12 23:02:37 -0700

    Rendering images from backend!

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,10 @@
+import Post from './Post';
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +20,7 @@ function App() {
         >
           Learn React
         </a>
+        <Post />
       </header>
     </div>
   );
--- frontend/src/Post.tsx
+++ frontend/src/Post.tsx
@@ -4,18 +4,21 @@ import axios from 'axios'
 type Base64 = string;
 
 export default class Post extends React.Component {
+    state: { data: Array<Base64> } = { data: [] };
+
+    componentDidMount(): void {
+        axios.get(`http://127.0.0.1:8000/`).then(res => {
+            this.setState(res.data);
+            console.log(res.data, this.state);
+            this.forceUpdate();
+        });
+    }
 
     render(): React.ReactNode {
-        console.log("I HAVE MADE A BIG THINGY");
-        let images: Array<Base64> = [];
-        axios.get(`http://127.0.0.1:8000/`).then(res => {
-            images = res.data;
-            console.log(images);
-        });
         return (
             <ul>
                 asdofiajsodfij
-                { images.map((image) => <img src={`data:image/jpeg;base64,${image}`} />)}
+                { this.state.data.map((image) => <img key={`${image.slice(image.length-10)}`} src={`data:image/jpeg;base64,${image}`} />)}
             </ul>
         )
     }
