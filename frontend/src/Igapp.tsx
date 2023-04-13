32fb14e636de88d777ed17f98f8a1f738b2055d2
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-04-12 23:32:45 -0700

    Copy leocosta1/instagram-clone files

---
+++ frontend/src/Igapp.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+export default class Igapp extends React.Component {
+    // write a UI resembling instagram
+
+    render(): React.ReactNode {
+        return (<div className="Igapp"> </div>)
+    }
+}
