4d5e4790fa1bff422fae26852d7cb44635aff8b6
potatoboiler <61665896+potatoboiler@users.noreply.github.com>
2023-04-13 04:44:00 -0700

    Load SD images into instagram template!

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import Post from './Post';
 
 const posts = ReactDOM.createRoot(
